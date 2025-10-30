2a91f222ffca44476f0b76e99a8b45ef8b232f9e
PlutonianProgrammer <plutonianprogrammer@gmail.com>
2025-10-29 19:39:29 -0500

    Added bubble-div component and styled stats page

--- frontend/src/routes/sign-in/sign-in.component.jsx
+++ frontend/src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,16 @@
+import "./sign-in.styles.scss";
+
 const SignIn = () => {
-  return <h1>SignIn Page</h1>;
+  return (
+    <div className='sign-in-page-container'>
+      <div className='sign-up-container'>
+        <h2>SIGN UP</h2>
+      </div>
+      <div className='sign-in-container'>
+        <h2>SIGN IN</h2>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- frontend/src/routes/stats/stats.component.jsx
+++ frontend/src/routes/stats/stats.component.jsx
@@ -1,5 +1,24 @@
+import BubbleDiv from "../../components/bubble-div/bubble-div.component";
+
+import "./stats.styles.scss";
+
 const Stats = () => {
-  return <h1>Stats Page</h1>;
+  return (
+    <div className='stats-page-container'>
+      <div className='left-side'>
+        <h1>My Stats</h1>
+        <BubbleDiv>
+          <h3>Best WPM: |FILLER|</h3>
+          <h3>Avg WPM: |FILLER|</h3>
+          <h3>My Placement: |FILLER|</h3>
+        </BubbleDiv>
+      </div>
+
+      <div className='graph-container'></div>
+
+      <div className='right-side'></div>
+    </div>
+  );
 };
 
 export default Stats;
