cc4d9645ef63a4cde9cd7abf4f16dac42cbdecdc
PlutonianProgrammer <plutonianprogrammer@gmail.com>
2025-11-27 12:58:37 -0600

    Leaderboard semi-functional

--- frontend/src/components/podium/podium.component.jsx
+++ frontend/src/components/podium/podium.component.jsx
@@ -4,9 +4,9 @@ import "./podium.styles.scss";
 const Podium = ({ firstPlace, secondPlace, thirdPlace }) => {
   return (
     <div className='podium-container'>
-      {secondPlace && <PodiumPiece name={secondPlace.name} placement='2' />}
-      {firstPlace && <PodiumPiece name={firstPlace.name} placement='1' />}
-      {thirdPlace && <PodiumPiece name={thirdPlace.name} placement='3' />}
+      {secondPlace && <PodiumPiece name={secondPlace.username} placement='2' />}
+      {firstPlace && <PodiumPiece name={firstPlace.username} placement='1' />}
+      {thirdPlace && <PodiumPiece name={thirdPlace.username} placement='3' />}
     </div>
   );
 };
