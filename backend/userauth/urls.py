b33048db8897a7642b9f75049991e4894168760b
PlutonianProgrammer <plutonianprogrammer@gmail.com>
2025-11-27 12:58:37 -0600

    Leaderboard semi-functional

--- backend/userauth/urls.py
+++ backend/userauth/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import update_user_score_records
+from .views import update_user_score_records, leaderboard_placement_best, leaderboard_placement_avg
 
 urlpatterns = [
     #path('info/', include('userauth.urls')),
     path('submit-score/', update_user_score_records.as_view()),
+    path('get-leaderboard-placement-best/', leaderboard_placement_best),
+    path('get-leaderboard-placement-avg/', leaderboard_placement_avg),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
--- frontend/src/routes/leaderboard/leaderboard.component.jsx
+++ frontend/src/routes/leaderboard/leaderboard.component.jsx
@@ -1,5 +1,7 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useState } from "react";
+
+import { AuthContext } from "../../contexts/AuthContext";
 
 import BubbleDiv from "../../components/bubble-div/bubble-div.component";
 import Podium from "../../components/podium/podium.component";
@@ -7,24 +9,53 @@ import Podium from "../../components/podium/podium.component";
 import "./leaderboard.styles.scss";
 
 const Leaderboard = () => {
-  const [firstPlace, setFirstPlace] = useState(null);
-  const [secondPlace, setSecondPlace] = useState(null);
-  const [thirdPlace, setThirdPlace] = useState(null);
+  const [firstPlace, setFirstPlace] = useState({
+    username: "harold",
+  });
+  const [secondPlace, setSecondPlace] = useState({
+    username: "jim",
+  });
+  const [thirdPlace, setThirdPlace] = useState({
+    username: "bert",
+  });
+  const [placement, setPlacement] = useState(0);
+  const [participants, setParticipants] = useState(0);
+
+  const { user, accessToken, refreshAccessToken } = useContext(AuthContext);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch("http://localhost:8000/api/users/");
-      if (response.ok) {
-        try {
-          const data = await response.json();
-          setFirstPlace(data[0]);
-          setSecondPlace(data[1]);
-          setThirdPlace(data[2]);
-        } catch (e) {
-          console.log(e);
+      let response = await fetch(
+        "http://localhost:8000/userauth/get-leaderboard-placement-best/",
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
         }
-      } else {
-        //TODO: HANDLE ERRORS
+      );
+      if (!response.ok) {
+        const newToken = refreshAccessToken();
+        response = await fetch(
+          "http://localhost:8000/userauth/get-leaderboard-placement-best/",
+          {
+            headers: { Authorization: `Bearer ${newToken}` },
+          }
+        );
+        if (!response.ok) {
+          console.log("FETCHING LEADERBOARD FAILED");
+          return;
+        }
+      }
+      try {
+        console.log("LOGGING:");
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        setFirstPlace(data.first);
+        setSecondPlace(data.second);
+        setThirdPlace(data.third);
+        setPlacement(data.placement);
+        setParticipants(data.participants);
+      } catch (e) {
+        console.log(e);
       }
     }
 
@@ -33,13 +64,17 @@ const Leaderboard = () => {
 
   return (
     <div className='leaderboard-container'>
-      <BubbleDiv className='personal-placement-container'>
-        <h1>Your Placement:</h1>
-        <p>1st</p>
-        <h2>Out Of</h2>
-        <p>300 Participants</p>
+      <BubbleDiv className='leaderboard-my-stats'>
+        <h1>Leaderboard</h1>
+        {user && (
+          <ul>
+            <li>My Best WPM: {user.best_wpm}</li>
+            <li>My Average WPM: {user.avg_wpm}</li>
+            <li>Placement: {placement}</li>
+            <li>Out of {participants} participants</li>
+          </ul>
+        )}
       </BubbleDiv>
-
       <div className='podium-container'>
         <Podium
           firstPlace={firstPlace}
