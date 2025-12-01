acfc662dc109cac85e11ef89e1eedca345610768
PlutonianProgrammer <plutonianprogrammer@gmail.com>
2025-12-01 08:34:21 -0600

    Tweaks to selectively restricted areas, polishing of leaderboard placement

--- frontend/src/routes/stats/stats.component.jsx
+++ frontend/src/routes/stats/stats.component.jsx
@@ -8,23 +8,26 @@ import "./stats.styles.scss";
 
 const Stats = () => {
   const { user } = useContext(AuthContext);
-
-  return (
-    <div className='stats-page-container'>
-      <div className='my-stats-container'>
-        {user && (
-          <BubbleDiv>
-            <h1>My Stats</h1>
-            <h3>Best WPM: {user.best_wpm.toFixed(2)} wpm</h3>
-            <h3>Avg WPM: {user.avg_wpm.toFixed(2)} wpm</h3>
-          </BubbleDiv>
-        )}
+  if (user) {
+    return (
+      <div className='stats-page-container'>
+        <div className='my-stats-container'>
+          {user && (
+            <BubbleDiv>
+              <h1>My Stats</h1>
+              <h3>Best WPM: {user.best_wpm.toFixed(2)} wpm</h3>
+              <h3>Avg WPM: {user.avg_wpm.toFixed(2)} wpm</h3>
+            </BubbleDiv>
+          )}
+        </div>
+        <div className='graph-container'>
+          <div className='graph-placeholder' />
+        </div>
       </div>
-      <div className='graph-container'>
-        <div className='graph-placeholder' />
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <h1>You Must Be Signed In To Use This Page</h1>;
+  }
 };
 
 export default Stats;
