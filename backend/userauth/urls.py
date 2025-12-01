5532caba8d146542aba33048024b63924d5cbbb3
PlutonianProgrammer <plutonianprogrammer@gmail.com>
2025-12-01 15:12:08 -0600

    Rudimentary functioning graph functionality added

--- backend/userauth/urls.py
+++ backend/userauth/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import update_user_score_records, leaderboard_placement_best, leaderboard_placement_avg
+from .views import update_user_score_records, leaderboard_placement_best, leaderboard_placement_avg, get_user_graph
 
 urlpatterns = [
     #path('info/', include('userauth.urls')),
     path('submit-score/', update_user_score_records.as_view()),
     path('get-leaderboard-placement-best/', leaderboard_placement_best),
     path('get-leaderboard-placement-avg/', leaderboard_placement_avg),
+    path('get-user-graph/', get_user_graph.as_view()),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
