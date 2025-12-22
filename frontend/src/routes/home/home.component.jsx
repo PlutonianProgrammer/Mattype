07c7d841e6627a0065b8521deb7a3ecf84d0b132
PlutonianProgrammer <plutonianprogrammer@gmail.com>
2025-12-22 14:06:20 -0600

    More re-styling

--- frontend/src/routes/home/home.component.jsx
+++ frontend/src/routes/home/home.component.jsx
@@ -1,5 +1,5 @@
-import { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import { AuthContext } from "../../contexts/AuthContext";
 
@@ -7,7 +7,8 @@ import BubbleDiv from "../../components/bubble-div/bubble-div.component";
 
 import "./home.styles.scss";
 const Home = () => {
-  const { login, user } = useContext(AuthContext);
+  const { guestLogin } = useContext(AuthContext);
+  const navigate = useNavigate();
   return (
     <div className='home-container'>
       <h1>MatType</h1>
@@ -15,13 +16,13 @@ const Home = () => {
         Typing Practice Application with Leaderboard and Graph Functionality
       </p>
 
-      <BubbleDiv>
-        <Link
-          to='/play'
-          onClick={() => !user && login("UniversalGuest", "GuestPassword123")}
-        >
-          Demo Now
-        </Link>
+      <BubbleDiv
+        onClick={() => {
+          guestLogin();
+          navigate("play/");
+        }}
+      >
+        Demo Now
       </BubbleDiv>
     </div>
   );
