ae09820fd7aacba3e075b9fc520992ed06a61c7b
PlutonianProgrammer <plutonianprogrammer@gmail.com>
2026-01-31 13:01:36 -0600

    Migrations on github

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=os.environ.get("DATABASE_PUBLIC_URL")
     )
 }
 
